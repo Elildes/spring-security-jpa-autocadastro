bec34df6a1a77cd54eaa968b665da776a7e528e1
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2024-07-08 19:37:12 -0300

    fix: ajusta senha

--- src/main/java/br/com/ufrn/imd/securityexample/PasswordEncoderUtil.java
+++ src/main/java/br/com/ufrn/imd/securityexample/PasswordEncoderUtil.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoderUtil {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "itamir";
+        String rawPassword = "elildes";
         String encodedPassword = encoder.encode(rawPassword);
         System.out.println("Encoded password: " + encodedPassword);
     }
